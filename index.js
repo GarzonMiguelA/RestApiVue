5211f5f46363785c1cca4d60a454e61a7ebedc86
GaronMiguelA <miguel.garzon.7e7@itb.cat>
2025-01-21 19:53:34 +0100

    CRUD with JSON

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ readData();
 
 console.log(readData());
 
+const writeData = (data) => {
+    try {
+        fs.writeFileSync("./db.json", JSON.stringify(data));
+    
+    }
+    catch (err) {
+        console.error(err);
+    
+    }
+}
+
 app.listen(3000, () => {
   console.log("Servidor en marcha en http://localhost:3000");
 }
@@ -48,23 +59,57 @@ app.get("/books/:id", (req, res) => {
 });
 
 
-const writeData = (data) => {
-    try {
-        fs.writeFileSync("./db.json", JSON.stringify(data));
-    
-    }
-    catch (err) {
-        console.error(err);
-    
-    }
-}
+
 
 /* Para añadir uno nuevo */
 app.post("/books", (req, res) => {
     const data = readData();
-    const newBook = req.body;
+    /* Body es lo que esta entre {} en request.http */
+    const body = req.body;
+    const newBook = {
+        id: data.books.length + 1,
+        ...body,
+        /* Podria ser tambien
+        ...req.body
+        pero mejor por partes */
+    }
     data.books.push(newBook);
+
     writeData(data);
+
     res.json(newBook);
 }
 );
+
+/* Actualizamos libro: */
+app.put("/books/:id", (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const id = parseInt(req.params.id);
+
+    const bookIndex = data.books.findIndex(book =>  book.id === id);
+
+    data.books[bookIndex] = {
+        ...data.books[bookIndex],
+        ...body,
+    }
+
+    writeData(data);
+    res.json({
+        message: "Libro actualizado",
+    });
+});
+
+app.delete("/books/:id", (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+
+    const bookIndex = data.books.findIndex(book => book.id === id);
+
+    data.books.splice(bookIndex, 1);
+
+    writeData(data);
+    res.json({
+        message: "Libro eliminado korrektamente",
+    });
+});
